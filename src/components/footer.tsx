e6b6b22741055e75f04a6e56dc4871fce1f986c0
PawanDwi12 <pawan.dwivedi@redbaton.in>
2025-04-13 02:28:44 +0530

    number added

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -69,7 +69,7 @@ export default function Footer() {
                   <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M17.657 16.657L13.414 20.9a1.998 1.998 0 01-2.827 0l-4.244-4.243a8 8 0 1111.314 0z" />
                   <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M15 11a3 3 0 11-6 0 3 3 0 016 0z" />
                 </svg>
-                <span className="text-green-100">123 Herbal Street, Mumbai, India</span>
+                <span className="text-green-100">Delhi , India</span>
               </li>
               <li className="flex items-start">
                 <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 mr-2 text-green-300" fill="none" viewBox="0 0 24 24" stroke="currentColor">
